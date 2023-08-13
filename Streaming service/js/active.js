6d74da49d3ee58e1400a14a4f98c983eff8af075
Zaven <124444187+Zaven-99@users.noreply.github.com>
2023-08-13 16:21:51 +0300

    Update active.js

--- Streaming service/js/active.js
+++ Streaming service/js/active.js
@@ -11,5 +11,21 @@ const mouseOverAndOut = (selector) => {
     });
   });
 };
-
 mouseOverAndOut(".menu-navigation");
+
+function active(selector) {
+  let menuNav = document.querySelectorAll(selector);
+  menuNav.forEach((item) => {
+    item.addEventListener("click", (e) => {
+       
+      menuNav.forEach((item) => {
+        item.classList.remove("active1");
+      });
+
+     
+      item.classList.add("active1");
+    });
+  });
+}
+active(".menu-nav");
+active(".about-music_navigation");
