3cc1be49acdaf44085e617fa5409bcc1b08edda0
Zaven <124444187+Zaven-99@users.noreply.github.com>
2023-08-31 22:13:09 +0300

    Update modal2.js

--- Streaming service/js/modal2.js
+++ Streaming service/js/modal2.js
@@ -1,19 +1,18 @@
 function modalWindow(selector) {
-    let user = document.querySelector(".user-img"),
-      modal = document.querySelector(selector),
-      div = document.querySelector(".main");
-  
-    user.addEventListener("click", () => {
-      modal.style.display = "block";
-      modal.classList.add("show");
-      modal.classList.remove("hide");
-       
-    });
-  
-    document.addEventListener("mousedown", (event) => {
-      if (!modal.contains(event.target)) {
-        modal.style.display = "none";
-      }
-    });
-  }
-  modalWindow(".modal-user");
+  let user = document.querySelector(".user-img"),
+    modal = document.querySelector(selector),
+    div = document.querySelector(".main");
+
+  user.addEventListener("click", () => {
+    modal.style.display = "block";
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+  });
+
+  document.addEventListener("mousedown", (event) => {
+    if (!modal.contains(event.target)) {
+      modal.style.display = "none";
+    }
+  });
+}
+modalWindow(".modal-user");
