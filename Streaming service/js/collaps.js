78539b4649ef2e43efdc2148a222c103d14ea492
Zaven <124444187+Zaven-99@users.noreply.github.com>
2023-08-31 22:12:25 +0300

    Update collaps.js

--- Streaming service/js/collaps.js
+++ Streaming service/js/collaps.js
@@ -1,22 +1,46 @@
-function collaps() {
+function burger() {
   let shareTab = document.querySelectorAll(".share2");
-  let tabInPodcats = document.querySelectorAll(".tabInPodcats");
+  let allTabContents = document.querySelectorAll(".tabInPodcats");
 
-  shareTab.forEach((share) => {
-    share.addEventListener("click", (e) => {
-      tabInPodcats.forEach((item) => {
-        if (item.style.display == "block") {
-          item.style.display = "none";
-          item.classList.remove("show");
-          item.classList.remove("active");
-        } else {
-          item.style.display = "block";
-          item.classList.add("show");
-          item.classList.add("active");
-        }
-      });
+  // Закрываем все окна вкладок
+  function closeAllTabs() {
+    allTabContents.forEach((content) => {
+      content.classList.remove("active-tab");
     });
+
+    shareTab.forEach((icon) => {
+      icon.classList.remove("active");
+    });
+  }
+
+  shareTab.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      let t = e.currentTarget;
+      let drop = t.closest(".podcast-item").querySelector(".tabInPodcats");
+
+      // Если текущий share уже активен, то просто закрываем его
+      if (t.classList.contains("active")) {
+        drop.classList.remove("active-tab");
+        t.classList.remove("active");
+      } else {
+        // Закрываем все окна вкладок перед открытием новой
+        closeAllTabs();
+
+        // Открываем текущую вкладку
+        drop.classList.add("active-tab");
+        t.classList.add("active");
+        drop.classList.add("show");
+      }
+    });
+  });
+
+  // Закрываем вкладку по клику на подложку
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest(".tabInPodcats") && !e.target.closest(".share2")) {
+      closeAllTabs();
+    }
   });
 }
 
-collaps();
+burger();
